705354704f9053b0c7b07bb8b8b2d166e10c575f
ordinary-developer <merely.ordinary.developer@gmail.com>
2016-08-05 13:50:16 +0300

    add an example with deadlock
    add an example with a deadlock using mutexes

---
+++ code/part_1/08-lock_with_deadlock/main.cpp
@@ -0,0 +1,22 @@
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+int main() {
+    auto call = [](std::mutex& first, std::mutex& second) {
+        first.lock();
+        second.lock();
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        first.unlock();
+        second.unlock();
+    };
+
+    std::mutex first;
+    std::mutex second;
+    std::thread firstThread(call, std::ref(first), std::ref(second));
+    std::thread secondThread(call, std::ref(second), std::ref(first));
+    firstThread.join();
+    secondThread.join();
+
+    return 0;
+}
